8c2fbbe4cd685aa1969a1c2d992810df384ed3c2
Jefe4 <82603665+Jefe4@users.noreply.github.com>
2024-02-15 18:56:04 -0500

    Update BaseballPlayer.java

--- Java/Lab2/src/problem1/BaseballPlayer.java
+++ Java/Lab2/src/problem1/BaseballPlayer.java
@@ -1,10 +1,11 @@
 package problem1;
-
+// Java class for the Baseball player as it would add information about stats
 public class BaseballPlayer extends CommonPlayerInfo {
+// adds the player stats into the baseball player class
 	private double battingAverage;
 	private double onBasePercentage;
 	private int RBIs;
-	
+// public class that will store the basic player info
 	public BaseballPlayer(String name, int jerseyNumber, double salary, double battingAverage, double onBasePercentage, int RBIs) {
 		super(name, jerseyNumber, salary);
 		this.battingAverage = battingAverage;
