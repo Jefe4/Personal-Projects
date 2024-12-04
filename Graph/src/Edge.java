560a99ef6a5cf63a9587234e582e5b2baa9e2c84
Jefe4 <82603665+Jefe4@users.noreply.github.com>
2024-12-04 15:44:11 -0500

    fix 1

--- Graph/src/Edge.java
+++ Graph/src/Edge.java
@@ -1,12 +1,12 @@
-/*
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
 
 class Edge<L> {
-    private int[][] adjMatrix;
-    public int numVertices;
-    L connectedVetex;
+    // Removed unused fields
+    // private int[][] adjMatrix;
+    // public int numVertices;
+    L connectedVertex;
     int liters;
 
     public Edge(L v, int liters) {
@@ -16,104 +16,7 @@ class Edge<L> {
 
     @Override
     public String toString() {
-        return "(" + connectedVetex + ", " + liters + ")";
+        return "(" + connectedVertex + ", " + liters + ")";
     }
 }
-    public class GraphWeighted<L> {
-        Map<L, LinkedList<Edge<L>>> adjMatrix = new HashMap<>();
-        boolean directed;
 
-        public GraphWeighted() {
-            directed = false;
-        }
-
-        public GraphWeighted(boolean d) {
-            directed = d;
-        }
-
-    }
-
-
-
-    //Add the edges
-    // this will be the cost of liters
-    // function to update the adjacency
-    // matrix for edge insertion
-    public void addEdge(L i, L j , int liters) {
-        //linked list for liters
-
-        adjMatrix.putIfAbsent(i, new LinkedList<>());
-        adjMatrix.putIfAbsent(j, new LinkedList<>());
-        Edge<L> edge1 = new Edge<>(j, liters);
-        adjMatrix.get(i).add(edge1);
-        if (!directed) {
-            Edge<L> edge2 = new Edge<>(i, liters);
-            adjMatrix.get(j).add(edge2);
-        }
-
-        }
-        public boolean DFS(L i, L j ) {
-            HashMap<L,Boolean> visited = new HashMap<>();
-            return dfsHelper(i , j , visited);
-        }
-        private boolean dfsHelper(L v, L j, HashMap<L, Boolean> visited) {
-            if(v == j)
-                return true;
-            visited.put(v,true);
-            for(Edge<L> edge : adjMatrix.get(v)) {
-                L d = edge.connectedVetex;
-                if(visited.get(d) == null)
-                    return dfsHelper(d, j , visited);
-            }
-            return false;
-        }
-        public void printGraph() {
-            for (L key : adjMatrix.keySet()) {
-                System.out.println(key + ", " + adjMatrix.get(key));
-            }
-        }
-
-
-    // prints the graph
-    public static void main(String args[] ){
-        char A = '0';
-        char B = '1';
-        char C = '2';
-        char D = '3';
-        char E = '4';
-        char F = '5';
-        char G = '6';
-        char H = '7';
-        char I = '8';
-        char J = '9';
-        char K = '1'+'0';
-        char L = '1'+'1';
-
-        int numVertices = 12, i = 11, j=11;
-        Room graph = new Room(12);
-
-        graph.addEdge(0,1,5); //5);
-        graph.addEdge(0,3,1);//1
-        graph.addEdge(0,4,4);//4
-        graph.addEdge(1,2,7);//7
-        graph.addEdge(1,7,4);//4
-        graph.addEdge(2,6,11);//11
-        graph.addEdge(3,2,3);//3
-        graph.addEdge(4,5,1);//1
-        graph.addEdge(5,1,3);//3
-        graph.addEdge(6,4,17);//17
-        graph.addEdge(6,7,6);//6
-        graph.addEdge(6,9,4);//4
-        graph.addEdge(7,8,5);//5
-        graph.addEdge(7,9,9);//9
-        graph.addEdge(7,10,7);//7
-        graph.addEdge(8,4,12);//12
-        graph.addEdge(9,10,8);//8
-        graph.addEdge(10,11,2);//2
-        graph.addEdge(11,8,5);//5
-        graph.printGraph();
-
-    }
-
-}
-*/
--- Graph/src/Room.java
+++ Graph/src/Room.java
@@ -1,22 +1,16 @@
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.io.*;
 import java.util.*;
+
 public class Room {
     private int[][] adjMatrix;
     private int numVertices;
     int liters;
     private boolean visit[];
 
-
     // Initialize the matrix
     public Room(int numVertices) {
-        //this will be A,B,C etc.
         this.numVertices = numVertices;
         adjMatrix = new int[numVertices][numVertices];
         visit = new boolean[numVertices];
-
 
         // zeros the matrix
         for (int i = 0; i < numVertices; i++) {
@@ -26,158 +20,99 @@ public class Room {
         }
     }
 
-    //Add the edges
-    // this will be the cost of liters
-    // function to update the adjacency
-    // matrix for edge insertion
+    // Add the edges
     public void addEdge(int i, int j, int liters) {
-        //linked list for liters
-
-
-        //adds an edge for directed graph
         if ((i < 0) || (i >= numVertices)) {
-            System.out.printf("Vertices" + i + "does not exist.");
+            System.out.printf("Vertices " + i + " does not exist.\n");
         }
         if ((j < 0) || (j >= numVertices)) {
-            System.out.printf("Vertices" + j + "does not exist.");
+            System.out.printf("Vertices " + j + " does not exist.\n");
         }
         if (i == j) {
             System.out.println("Same vertex");
         } else {
-            adjMatrix[i][j] = 1;
-            liters = 1;
-
+            adjMatrix[i][j] = liters;
         }
-
-
     }
 
-    //remove the edges
-    public void removeEdge(int i, int j, int liters) {
+    // Remove the edges
+    public void removeEdge(int i, int j) {
         if ((i < 0) || (i >= numVertices)) {
-            System.out.printf("Vertices" + i + "does not exist.");
+            System.out.printf("Vertices " + i + " does not exist.\n");
         }
         if ((j < 0) || (j >= numVertices)) {
-            System.out.printf("Vertices" + j + "does not exist.");
+            System.out.printf("Vertices " + j + " does not exist.\n");
         }
         if (i == j) {
             System.out.println("Same vertex");
         } else {
             adjMatrix[i][j] = 0;
-            liters = 0;
-
         }
-
     }
 
-}
-    void DFS(int v , boolean visited[]){
-        // mark the current node as visited
+    // DFS traversal
+    void DFS(int v, boolean visited[]) {
         visited[v] = true;
-        visit(v);
-        //recursion for the vertices next to the dfs
-        for(Element visited: v){
-            System.our.println(visited);
+        System.out.print(v + " ");
 
-        }
-
-        for(int i : adjMatrix.get(v)) {
-            if(!visited[i])
+        for (int i = 0; i < numVertices; i++) {
+            if (adjMatrix[v][i] != 0 && !visited[i]) {
                 DFS(i, visited);
+            }
         }
-        }
-
-
-    void DFST(int v){
-        // mark every other vertices that is not visited as false
-        boolean visited[] = new boolean[v];
-        // call the robot to print the traversal
-        DFS(v,visited);
     }
 
+    void DFST(int v) {
+        boolean visited[] = new boolean[numVertices];
+        DFS(v, visited);
+    }
 
-    //print the matrix
-
-
-    public void printGraph(){
-        for(int i = 0; i < numVertices; i ++){
-            for(int j = 0; j < numVertices; j++) {
-                    System.out.print(adjMatrix[i][j] + " ");
-            }// first vertex
+    // Print the matrix
+    public void printGraph() {
+        for (int i = 0; i < numVertices; i++) {
+            for (int j = 0; j < numVertices; j++) {
+                System.out.print(adjMatrix[i][j] + " ");
+            }
             System.out.println();
         }
 
-        for(int i = 0; i < numVertices ; i++){
-            System.out.print("Room" + i + " is connected to: ");
-            for(int j = 0; j < numVertices; j++) {
-
-                    if (adjMatrix[i][j]== 1) {
-                        System.out.print(j + " ");
-                    }
-
-            } // second vertex
+        for (int i = 0; i < numVertices; i++) {
+            System.out.print("Room " + i + " is connected to: ");
+            for (int j = 0; j < numVertices; j++) {
+                if (adjMatrix[i][j] != 0) {
+                    System.out.print(j + " ");
+                }
+            }
             System.out.println();
         }
+    }
 
-
-}
-
-    // prints the graph
-    public static void main(String args[] ){
-        char A = 0;
-        char B = 1;
-        char C = 2;
-        char D = 3;
-        char E = 4;
-        char F = 5;
-        char G = 6;
-        char H = 7;
-        char I = 8;
-        char J = 9;
-        char K = 10;
-        char L = 11;
-    List<Integer> one = new LinkedList<Integer>();
-
-        one.add(A);
-        one.add(B);
-
-
-        one.add(C);
-        one.add(D);
-        one.add(E);
-        one.add(F);
-        one.add(G);
-        one.add(H);
-        one.add(I);
-        one.add(J);
-        one.add(K);
-        one.add(L);
-
-        int numVertices = 12, i = 11, j=11;
+    // Main method
+    public static void main(String args[]) {
         Room graph = new Room(12);
-  
-        graph.addEdge(0,1,5); //5);
-        graph.addEdge(0,3,1);//1
-        graph.addEdge(0,4,4);//4
-        graph.addEdge(1,2,7);//7
-        graph.addEdge(1,7,4);//4
-        graph.addEdge(2,6,11);//11
-        graph.addEdge(3,2,3);//3
-        graph.addEdge(4,5,1);//1
-        graph.addEdge(5,1,3);//3
-        graph.addEdge(6,4,17);//17
-        graph.addEdge(6,7,6);//6
-        graph.addEdge(6,9,4);//4
-        graph.addEdge(7,8,5);//5
-        graph.addEdge(7,9,9);//9
-        graph.addEdge(7,10,7);//7
-        graph.addEdge(8,4,12);//12
-        graph.addEdge(9,10,8);//8
-        graph.addEdge(10,11,2);//2
-        graph.addEdge(11,8,5);//5
+
+        graph.addEdge(0, 1, 5);
+        graph.addEdge(0, 3, 1);
+        graph.addEdge(0, 4, 4);
+        graph.addEdge(1, 2, 7);
+        graph.addEdge(1, 7, 4);
+        graph.addEdge(2, 6, 11);
+        graph.addEdge(3, 2, 3);
+        graph.addEdge(4, 5, 1);
+        graph.addEdge(5, 1, 3);
+        graph.addEdge(6, 4, 17);
+        graph.addEdge(6, 7, 6);
+        graph.addEdge(6, 9, 4);
+        graph.addEdge(7, 8, 5);
+        graph.addEdge(7, 9, 9);
+        graph.addEdge(7, 10, 7);
+        graph.addEdge(8, 4, 12);
+        graph.addEdge(9, 10, 8);
+        graph.addEdge(10, 11, 2);
+        graph.addEdge(11, 8, 5);
+
         graph.printGraph();
-        System.out.println("FRIEND-BOT-BFS1 visited"+numVertices+"and successfully refrained from destorying any humans, wink wink. We made a new friend! <3 <3 <3! The fuel usage was"+ graph.liters+"liters");
+        System.out.println("FRIEND-BOT-BFS1 visited " + graph.numVertices + " and successfully refrained from destroying any humans, wink wink. We made a new friend! <3 <3 <3! The fuel usage was " + graph.liters + " liters");
         graph.DFST(0);
     }
-
 }
